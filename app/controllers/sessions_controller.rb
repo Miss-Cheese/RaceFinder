21f04ce78ff705ea18e069ef5b211ecb62a191c9
Edward Castillo <castilloedwarda@gmail.com>
2020-01-03 13:52:22 -0500

    Lotta css and stuff

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,16 @@
+
+
 class SessionsController < ApplicationController
     
     def welcome
     end
 
     def create
-        if params[:name] && params[:name] != ''
+        if  Runner.find_by(name: params[:name])
             session[:name] = params[:name]
             redirect_to "/home"
         else
+            flash[:notice] = "Invalid username"
             redirect_to "/welcome" 
         end
     end
